d2ca93276c54d09b222d8b6661465df5dd1d034c
Nathasha <nsoares@corpcloud.com.au>
2022-11-24 13:25:09 +0800

    lmb importante

---
+++ src/lmb.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Hearts } from "react-loader-spinner";
+
+export default function Lmb(props) {
+  alert(`Beijos procê! 😘`);
+
+  return (
+    <Hearts
+      height="80"
+      width="200"
+      color="pink"
+      ariaLabel="hearts-loading"
+      wrapperStyle={{}}
+      wrapperClass=""
+      visible={true}
+    />
+  );
+}
